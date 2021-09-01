9e52572fbd91d721849cc56d5db433cc9101265c
fred <ltfred@163.com>
2021-09-01 14:18:25 +0800

    feat: share

--- pkg/sync/share.go
+++ pkg/sync/share.go
@@ -1,54 +1,74 @@
-package sync
+package syncx
 
-import (
-	"sync"
+import "sync"
+
+type (
+	SharedCalls interface {
+		Do(key string, fn func() (interface{}, error)) (interface{}, error)
+		DoEx(key string, fn func() (interface{}, error)) (interface{}, bool, error)
+	}
+
+	call struct {
+		wg  sync.WaitGroup
+		val interface{}
+		err error
+	}
+
+	sharedGroup struct {
+		calls map[string]*call
+		lock  sync.Mutex
+	}
 )
 
-type ShareCalls interface {
-	Do(key string, fn func() (interface{}, error)) (interface{}, error)
-}
-
-type call struct {
-	wg sync.WaitGroup
-	val interface{}
-	err error
-}
-
-type sharedGroup struct {
-	calls map[string]*call
-	lock  sync.Mutex
-}
-
-
-func NewShareCalls() ShareCalls {
+func NewSharedCalls() SharedCalls {
 	return &sharedGroup{
 		calls: make(map[string]*call),
 	}
 }
 
-func (s *sharedGroup) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
-	s.lock.Lock()
-	d, ok := s.calls[key]
-	if ok {
-		s.lock.Unlock()
-		d.wg.Wait()
-		return d.val, d.err
+func (g *sharedGroup) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	c, done := g.createCall(key)
+	if done {
+		return c.val, c.err
 	}
 
-	c := new(call)
+	g.makeCall(c, key, fn)
+	return c.val, c.err
+}
+
+func (g *sharedGroup) DoEx(key string, fn func() (interface{}, error)) (val interface{}, fresh bool, err error) {
+	c, done := g.createCall(key)
+	if done {
+		return c.val, false, c.err
+	}
+
+	g.makeCall(c, key, fn)
+	return c.val, true, c.err
+}
+
+func (g *sharedGroup) createCall(key string) (c *call, done bool) {
+	g.lock.Lock()
+	if c, ok := g.calls[key]; ok {
+		g.lock.Unlock()
+		c.wg.Wait()
+		return c, true
+	}
+
+	c = new(call)
 	c.wg.Add(1)
-	s.calls[key] = c
-	s.lock.Lock()
+	g.calls[key] = c
+	g.lock.Unlock()
 
+	return c, false
+}
 
-	c.val, c.err = fn()
-
+func (g *sharedGroup) makeCall(c *call, key string, fn func() (interface{}, error)) {
 	defer func() {
-		s.lock.Unlock()
-		delete(s.calls, key)
-		s.lock.Unlock()
+		g.lock.Lock()
+		delete(g.calls, key)
+		g.lock.Unlock()
 		c.wg.Done()
 	}()
 
-	return c.val, c.err
+	c.val, c.err = fn()
 }
